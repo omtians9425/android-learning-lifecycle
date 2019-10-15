9c748870d4da44d83e47cf82e44a566dc4cdfa62
小俣幸之助 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-15 14:58:16 +0900

    create application subclass and import for manifest

---
+++ app/src/main/java/com/example/android/dessertpusher/PusherApplication.kt
@@ -0,0 +1,9 @@
+package com.example.android.dessertpusher
+
+import android.app.Application
+
+class PusherApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
